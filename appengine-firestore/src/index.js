2d8a47cee3c93eaeb53d6189575db257602354e3
jupemara <jumpei.arashi@arashike.com>
2022-05-31 17:25:53 +0900

    use also firebase-admin

--- appengine-firestore/src/index.js
+++ appengine-firestore/src/index.js
@@ -2,21 +2,35 @@
 
 const express = require('express'),
   app = express(),
-  { Firestore } = new require('@google-cloud/firestore'),
-  firestore = new Firestore();
+  { Firestore } = require('@google-cloud/firestore'),
+  firestore = new Firestore(),
+  { initializeApp, applicationDefault } = require('firebase-admin/app'),
+  { getFirestore } = require('firebase-admin/firestore');
 
 app.get('/', async (req, res) => {
   res.header('Content-Type', 'application/json');
   const contents = {
     path: '/',
   };
-  // use firestore
+  // use cloud firestore sdk
   try {
     const doc = firestore.doc('test-data/1');
-    contents.firestoreDoc = await doc.get();
+    contents.byCloudSdk = await doc.get();
   } catch (e) {
     console.error(`an error occurred on firestore: ${e}`);
   }
+  // use firebase admin
+  try {
+    initializeApp({
+      credential: applicationDefault(),
+    });
+    const db = getFirestore();
+    const ref = db.collection('test-data').doc('1');
+    contents.byFirebaseAdmin = await ref.get();
+  } catch (e) {
+    console.error(`an error occurred on firebase-admin: ${e}`);
+  }
+
   res.status(200).send(JSON.stringify(contents, null, 2)).end();
 });
 
