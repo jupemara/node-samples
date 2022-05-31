854a28dcc2b739036b22e9074752c23563334d57
jupemara <jumpei.arashi@arashike.com>
2022-05-31 16:55:04 +0900

    use @google-cloud/firestore

--- appengine-firestore/src/index.js
+++ appengine-firestore/src/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const express = require('express'),
-  app = express();
+  app = express(),
+  { Firestore } = new require('@google-cloud/firestore'),
+  firestore = new Firestore();
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello world').end();
+app.get('/', async (req, res) => {
+  res.header('Content-Type', 'application/json');
+  const contents = {
+    path: '/',
+  };
+  // use firestore
+  try {
+    const doc = firestore.doc('test-data/1');
+    contents.firestoreDoc = await doc.get();
+  } catch (e) {
+    console.error(`an error occurred on firestore: ${e}`);
+  }
+  res.status(200).send(JSON.stringify(contents, null, 2)).end();
 });
 
 app.listen(parseInt(process.env.PORT) || 8080, () => {
