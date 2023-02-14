2e1a33e46a0c10ba95ff70f33d759b24ea238eeb
jupemara <jumpei.arashi@arashike.com>
2023-02-14 17:16:25 +0900

    rollup + index code

---
+++ functions-memory-usage/src/index.ts
@@ -0,0 +1,33 @@
+import * as functions from '@google-cloud/functions-framework';
+import type { Request, Response } from '@google-cloud/functions-framework';
+import { Storage } from '@google-cloud/storage';
+import * as stream from 'stream';
+import * as fs from 'fs';
+
+const c = new Storage(),
+  BUCKET_NAME = process.env.BUCKET_NAME || '',
+  OBJECT_PATH = process.env.OBJECT_PATH || '';
+
+functions.http('x', fn);
+
+export async function fn(req: Request, res: Response): Promise<void> {
+  const f = await fs.promises.readFile('./gcp.png');
+  try {
+    await upload(BUCKET_NAME, OBJECT_PATH, f);
+    console.log('FIN');
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+}
+
+async function upload(bucket: string, path: string, content: Buffer) {
+  const suffix = new Date().getTime(),
+    f = c.bucket(bucket).file(`${path}-${suffix}`);
+  await stream.promises.pipeline(
+    stream.Readable.from(content),
+    f.createWriteStream({
+      resumable: false,
+    }),
+  );
+}
