952985a028eaf8a70d37850aa5dca0f936123d9d
jupemara <jumpei.arashi@arashike.com>
2023-07-29 02:34:01 +0900

    npm rm @google-cloud/functions-framework

--- pubsub-unwrap/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export const main = (req: Request, res: Response): void => {
-  console.log('body:', req.body);
-  console.log('headers:', req.headers);
-  res.status(200).send('ok');
-};
